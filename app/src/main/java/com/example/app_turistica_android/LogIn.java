a4f40dc2188c99310163ebebc4d129c583538ba8
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-03-12 11:19:04 +0100

    prueba registro OK firebase

--- app/src/main/java/com/example/app_turistica_android/LogIn.java
+++ app/src/main/java/com/example/app_turistica_android/LogIn.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.app_turistica_android.Maps.MapsActivity;
 
 public class LogIn extends AppCompatActivity {
 
-    Button btnInicio, btnRegistrar;
+    Button btnInicio;
+    ImageButton btnRegistrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

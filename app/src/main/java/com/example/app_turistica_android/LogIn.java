fa11ed9cd37486bb1008f423ce5289fcdbc4597b
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-03-12 11:09:43 +0100

    act creaccion MAPS

--- app/src/main/java/com/example/app_turistica_android/LogIn.java
+++ app/src/main/java/com/example/app_turistica_android/LogIn.java
@@ -2,13 +2,33 @@ package com.example.app_turistica_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
 
+    Button btnInicio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        instancias();
+        acciones();
+    }
+
+    private void acciones() {
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void instancias() {
+        btnInicio = findViewById(R.id.btnInicio);
     }
 }

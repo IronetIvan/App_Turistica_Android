c247cbac379632e1408d998edd4dab162ec09505
Adrian <adrian.olmo@cesjuanpabloseguno.es>
2020-03-13 15:40:33 +0100

    Login Completo

--- app/src/main/java/com/example/app_turistica_android/LogIn.java
+++ app/src/main/java/com/example/app_turistica_android/LogIn.java
@@ -1,19 +1,33 @@
 package com.example.app_turistica_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.app_turistica_android.Maps.MapsActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class LogIn extends AppCompatActivity {
 
     Button btnInicio;
     ImageButton btnRegistrar;
+    EditText nombre, password;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +41,7 @@ public class LogIn extends AppCompatActivity {
         btnInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LogIn.this, MapsActivity.class);
-                startActivity(intent);
+                IniciarSesion();
             }
         });
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
@@ -43,5 +56,57 @@ public class LogIn extends AppCompatActivity {
     private void instancias() {
         btnInicio = findViewById(R.id.btnInicio);
         btnRegistrar = findViewById(R.id.btnRegistrar);
+        nombre = findViewById(R.id.txtcorreo);
+        password = findViewById(R.id.txtpassword);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
     }
-}
+
+
+    private void IniciarSesion(){
+
+        final String email = nombre.getText().toString().trim();
+        String passwd = password.getText().toString().trim();
+
+        //Se comprueba que los textfield no esten vacios
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Se debe ingresar un email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(passwd)) {
+            Toast.makeText(this, "Falta ingresar la contraseña", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        progressDialog.setMessage("Limpiando la cancha...");
+        progressDialog.show();
+
+        //loguear usuario
+        firebaseAuth.signInWithEmailAndPassword(email, passwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()) {
+                            int pos = email.indexOf("@");
+                            String user = email.substring(0, pos);
+                            Toast.makeText(LogIn.this, "Bienvenido: " + nombre.getText(), Toast.LENGTH_LONG).show();
+                            Intent iniciarSesion = new Intent(LogIn.this, MapsActivity.class);
+                            LogIn.this.startActivity(iniciarSesion);
+
+
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {//si se presenta una colisión
+                                Toast.makeText(LogIn.this, "Ese usuario ya existe ", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(LogIn.this, "Usuario o contraseña incorrectos ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+    }

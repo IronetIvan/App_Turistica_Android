6f76d8d1a226ffcdf013da192ba4b8f08000a772
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-05-03 01:07:57 +0200

    act ayuda activity OK

--- app/src/main/java/com/example/app_turistica_android/Ayuda.java
+++ app/src/main/java/com/example/app_turistica_android/Ayuda.java
@@ -3,8 +3,13 @@ package com.example.app_turistica_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import pl.droidsonroids.gif.GifImageButton;
 
 public class Ayuda extends AppCompatActivity {
+    GifImageButton btnGif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,15 @@ public class Ayuda extends AppCompatActivity {
     }
 
     private void acciones() {
+        btnGif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Sin actualizaciones pendientes, gracias!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void instancias() {
+        btnGif= findViewById(R.id.btnGif);
     }
 }

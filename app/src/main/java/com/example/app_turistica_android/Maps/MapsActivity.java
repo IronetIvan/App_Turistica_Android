d192e38fdcfceb6eeac51639e3d5292d37606c67
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-05-03 00:26:08 +0200

    act correcion errores

--- app/src/main/java/com/example/app_turistica_android/Maps/MapsActivity.java
+++ app/src/main/java/com/example/app_turistica_android/Maps/MapsActivity.java
@@ -61,7 +61,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         instancias();
         acciones();
-        uid = getIntent().getExtras().getString("uid");
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()

c656db5d46dbb95982fbeda211189a57e51be200
Iván Redondo <ivan.redondo@cesjuanpablosegundo.es>
2020-05-07 11:41:00 +0200

    act metodo1

--- app/src/main/java/com/example/app_turistica_android/EditPerfil.java
+++ app/src/main/java/com/example/app_turistica_android/EditPerfil.java
@@ -87,6 +87,46 @@ public class EditPerfil extends AppCompatActivity {
         });
     }
 
+    private void cargarDatos1(){
+        final String uid = getIntent().getExtras().getString("uid");
+        final DatabaseReference nodoUsuarios = myRef.getDatabase().getReference().child("usuarios");
+        nodoUsuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Log.v("firebase",dataSnapshot.getKey());
+                // nulo
+                if (dataSnapshot.getKey().equals(uid)){
+                    Iterable<DataSnapshot> iterable = dataSnapshot.getChildren();
+                    Iterator<DataSnapshot> iterator = iterable.iterator();
+                    while (iterator.hasNext()){
+                        DataSnapshot actual = iterator.next();
+                        // el objeto json entero
+                        actual.getValue();
+
+                        String correo = actual.child("correo").getValue().toString();
+                        String usuario = actual.child("usuario").getValue().toString();
+                        String contraseña = actual.child("contraseña").getValue().toString();
+                        txtCorreo.setText(correo);
+                        txtUsuario.setText(usuario);
+                        txtPassword.setText(contraseña);
+
+                        /*String correo = dataSnapshot.child("correo").getValue().toString();
+                        String usuario = dataSnapshot.child("usuario").getValue().toString();
+                        String contraseña = dataSnapshot.child("contraseña").getValue().toString();
+                        */
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),"Error al cargar datos de usuario", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
 
 
 

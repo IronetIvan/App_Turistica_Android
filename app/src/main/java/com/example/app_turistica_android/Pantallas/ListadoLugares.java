00f5bf59fb4780215b1f708ff8f7350ac4f1123c
Adrian <adrian.olmo@cesjuanpabloseguno.es>
2020-05-08 17:09:04 +0200

    revision fragment

--- app/src/main/java/com/example/app_turistica_android/Pantallas/ListadoLugares.java
+++ app/src/main/java/com/example/app_turistica_android/Pantallas/ListadoLugares.java
@@ -1,12 +1,18 @@
 package com.example.app_turistica_android.Pantallas;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,6 +23,8 @@ import com.example.app_turistica_android.R;
 import com.example.app_turistica_android.utils.Lugares;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class ListadoLugares extends AppCompatActivity {
 
@@ -36,11 +44,44 @@ public class ListadoLugares extends AppCompatActivity {
         AdaptadorFirebase adaptadorFirebase = new AdaptadorFirebase(Lugares.class, R.layout.item_listado_lugares
         ,ListadoLugaresHolder.class, referencia,ListadoLugares.this);
 
-        /*AdaptadorFirebase adaptadorFirebase = new AdaptadorFirebase(Lugares.class, R.layout.recycler_listado_lugares
-        ,ListadoLugaresHolder.class, referencia,ListadoLugares.this);*/
-
         recyclerView.setAdapter(adaptadorFirebase);
         recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2,
                 LinearLayoutManager.VERTICAL, false));
     }
 }
+
+/*public class ListadoLugares extends Fragment {
+
+    RecyclerView recyclerView;
+    AdaptadorFirebase adaptadorFirebase;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference referencia = database.getReference("lugares").child("defecto");
+
+
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        adaptadorFirebase = new AdaptadorFirebase(Lugares.class, R.layout.item_listado_lugares,
+                ListadoLugaresHolder.class, referencia, ListadoLugares.this);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.recycler_listado_lugares, container, false);
+        recyclerView = v.findViewById(R.id.listadolugares);
+
+
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView.setAdapter();
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2,
+                LinearLayoutManager.VERTICAL, false));
+    }
+}*/
